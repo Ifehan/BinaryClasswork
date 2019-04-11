40f00fd996be3123df475d9ccd5982723c53999c
Ifehan <oduseifeoluwahan@gmail.com>
2019-04-11 11:57:36 +0100

    second file

---
+++ Binary/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Binary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConvertToBinary();
+        }
+        static void ConvertToBinary()
+        {
+            start: Console.Write("Enter your number here : ");
+            long i = long.Parse(Console.ReadLine()); 
+            Console.WriteLine($"Binary conversion is : {Binary(i)} \n");
+            goto start;
+        }
+        static string Binary(long j)
+        {
+            string binary = " ";
+            while (j > 0)
+            {
+                j = j / 2; binary = j % 2 + binary;
+            }
+            return binary;
+        }
+    }
+}
